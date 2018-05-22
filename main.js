7c64304c4535a7658681638468900ba987c77a35
Corey <coreydennis@rochester.rr.com>
2018-05-21 22:56:58 -0400

    started working on chord finding algorithm

--- main.js
+++ main.js
@@ -8,32 +8,39 @@ const blackKeys = [];
 const whiteNotes = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
 const blackNotes = ['', 'C#/Db', 'D#/Eb', '', 'F#/Gb', 'G#/Ab', 'A#/Bb'];
 
+const allNotes = ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B'];
+const scales = [];
+
 function initPiano() {
     var canvas = document.getElementById("piano");
-
-    //DEBUG
-    canvas.addEventListener("mousemove", (e) => {
-        var coords = document.getElementById('coords');
-        coords.innerHTML = 'x: ' + e.x + ', y: ' + e.y;
-    });
-    //
     
     canvas.width = pianoWidth;
     canvas.height = pianoHeight;
 
+    //Generate the values for the white keys
     if(whiteKeys.length == 0) {
-        //Generate the values for the white keys
         for(var i = 0; i < numOctaves * 7; i++) {
             whiteKeys.push({ x: i * keyWidth, y: 0, width: keyWidth, height: pianoHeight, selected: false, note: whiteNotes[i%7]})
         }
     }
 
+    //Generate the values for the black keys
     if(blackKeys.length == 0) {
-        //Generate the values for the black keys
         for(var i = 0; i < numOctaves * 7; i++) {
             //no black key on the first or fourth edge (left of C and left of F)
             if(i % 7 != 0 && i % 7 != 3)
                 blackKeys.push({ x: (keyWidth * i) - (keyWidth / 4), y: 0, width: keyWidth / 2, height: pianoHeight / 2, selected: false, note: blackNotes[i%7] });
+        }
+    }
+
+    //Generate all the scales used for finding chords
+    if(scales.length == 0) {
+        for(var i = 0; i < allNotes.length; i++) {
+            var scale = [];
+            for(var j = i; j < i + allNotes.length; j++) {
+                scale.push(allNotes[j % allNotes.length]);
+            }
+            scales.push(scale);
         }
     }
 
@@ -75,33 +82,6 @@ function initPiano() {
                 }
             });
         }
-        /*
-        whiteKeys.forEach(key => {
-            if(isIntersect(pos, key)) {
-                if(key.selected) {
-                    key.selected = false;
-                    drawPiano();
-                }
-                else {
-                    key.selected = true;
-                    drawPiano();
-                }
-            }
-        });
-
-        blackKeys.forEach(key => {
-            if(isIntersect(pos, key)) {
-                if(key.selected) {
-                    key.selected = false;
-                    drawPiano();
-                }
-                else {
-                    key.selected = true;
-                    drawPiano();
-                }
-            }
-        });
-        */
     });
 
     drawPiano();
@@ -144,6 +124,38 @@ function drawPiano() {
             ctx.fillText(key.note, key.x, key.y + (key.height / 1.5), key.width);
         });
     }
+}
+
+function searchForChord() {
+    var selectedNotes = [];
+    
+    //Get the selected notes
+    whiteKeys.forEach(key => {
+        if(key.selected)
+            selectedNotes.push(key.note);
+    });
+    blackKeys.forEach(key => {
+        if(key.selected)
+            selectedNotes.push(key.note);
+    });
+
+    if(selectedNotes.length < 3) {
+        alert("Select at least three notes to find a chord");
+        return;
+    }
+
+    var tryScales = [];
+
+    //Treat each selected note as the root to try to find chords
+    for(var i = 0; i < selectedNotes.length; i++) {
+        for(var j = 0; j < scales.length; j++) {
+            if(selectedNotes[i] == scales[j][0]) {
+                tryScales.push(scales[j]);
+            }
+        }
+    }
+
+    console.log(tryScales);
 }
 
 function isIntersect(point, rect) {
